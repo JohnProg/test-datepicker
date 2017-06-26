254da6384f7448d479b318f59865e8bf98ffe2f6
khanh@checkit.vn <khanh@checkit.vn>
2017-06-27 06:26:03 +0700

    Change test case for ios

--- src/features/ios.test.js
+++ src/features/ios.test.js
@@ -7,61 +7,76 @@ chaiAsPromised.transferPromiseness = wd.transferPromiseness
 chai.use(chaiAsPromised)
 chai.should()
 
-describe('iOS 10 DatePicker', function () {
-	let driver
-	this.timeout(300000)
+describe('iOS 10 DatePicker', function() {
+  let driver
+  this.timeout(300000)
 
-	before(() => {
-		driver = wd.promiseChainRemote(config.appium)
+  before(() => {
+    driver = wd.promiseChainRemote(config.appium)
 
-		return driver.init(config.ios10)
-	})
+    return driver.init(config.ios10)
+  })
 
-	after(() => {
-		return driver.quit()
-	})
+  after(() => {
+    return driver.quit()
+  })
 
-	it('should have a DatePicker', () => {
-		return driver
-			.elementsByClassName('android.widget.DatePicker')
-			.should.eventually.exist
-			.elementsByClassName('XCUIElementTypePickerWheel')
-			.then(els => {
-				els.length.should.equal(4)
-				return els
-			})
-			.first()
-			.getAttribute('value')
-			.should.become('Today')
+  it('should have a DatePicker', () => {
+    return driver
+      .elementsByClassName('android.widget.DatePicker')
+      .should.eventually.exist.elementsByClassName('XCUIElementTypePickerWheel')
+      .then(els => {
+        els.length.should.equal(4)
+        return els
+      })
+      .first()
+      .getAttribute('value')
+      .should.become('Today')
+  })
 
-	})
+  it('should set time 4:04 AM', () => {
+    return driver
+      .elementsByClassName('XCUIElementTypePickerWheel')
+      .second()
+      .sendKeys('4')
+      .getAttribute('value')
+      .should.become('4 o’clock')
+      .elementsByClassName('XCUIElementTypePickerWheel')
+      .third()
+      .sendKeys('04')
+      .getAttribute('value')
+      .should.become('04 minutes')
+      .elementsByClassName('XCUIElementTypePickerWheel')
+      .last()
+      .sendKeys('AM')
+      .getAttribute('value')
+      .should.become('AM ')
+  })
 
-	it('should set date Wed Jun 28', () => {
-		return driver
-			.elementsByClassName('XCUIElementTypePickerWheel').first()
-			.then(picker => driver.execute('mobile: selectPickerWheelValue', {
-					element: picker,
-					order: 'next'
-				})
-			)
-			.elementsByClassName('XCUIElementTypePickerWheel').first()
-			.getAttribute('value')
-			.should.become('Wed, Jun 28')
-	})
+  it('should select next 2 days', () => {
+    return driver
+      .elementsByClassName('XCUIElementTypePickerWheel')
+      .first()
+      .then(picker => {
+        return driver.execute('mobile: selectPickerWheelValue', {
+          element: picker,
+          order: 'next'
+        })
+      })
+      .sleep(1000)
+  })
 
-	it('should set time 4:04 AM', () => {
-		return driver
-			.elementsByClassName('XCUIElementTypePickerWheel').second()
-			.sendKeys('4')
-			.getAttribute('value')
-			.should.become('4 o’clock')
-			.elementsByClassName('XCUIElementTypePickerWheel').third()
-			.sendKeys('04')
-			.getAttribute('value')
-			.should.become('04 minutes')
-			.elementsByClassName('XCUIElementTypePickerWheel').last()
-			.sendKeys('AM')
-			.getAttribute('value')
-			.should.become('AM ')
-	})
+  it('should select a random day', () => {
+    return driver
+      .elementsByClassName('XCUIElementTypePickerWheel')
+      .first()
+      .then(picker =>
+        driver.execute('mobile: scroll', {
+          element: picker,
+          direction: 'down',
+          predicateString: 'Thu Jul 26'
+        })
+      )
+      .sleep(2000)
+  })
 })
